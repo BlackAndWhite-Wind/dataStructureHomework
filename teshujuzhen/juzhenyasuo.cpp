4e65c69ea11e78b8920e81276a26fb464105476a
Julia Jay <2020141460308@stu.scu.edu.cn>
2024-01-11 22:21:21 +0800

    1
    1

--- teshujuzhen/juzhenyasuo.cpp
+++ teshujuzhen/juzhenyasuo.cpp
@@ -1,33 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
+#include<iostream>
+#include<cstring>
+#include<cmath>
 
 using namespace std;
 
-bool isPrime(int num){
-	if(num<2) return false;
-	for(int i=2;i<=num/i;i++){
-		if(num%i==0) return false;
-	}
-	return true;
-}
-
-void printPrimesInRange(int a, int b){
-	vector<int> res;
-	for(int i=a;i<=b;i++){
-		if(isPrime(i)) res.push_back(i);
-	}
-	cout<<left;
-	for(int i=0;i<res.size();i++){
-		cout<<setw(5)<<res[i];
-		if((i+1)%5==0) cout<<endl;
-	}
-	cout<<right;
-}
-
 int main(){
-	int a,b; cin>>a>>b;
-	printPrimesInRange(a,b);
-	return 0;
+    for(int i=1;i<=pow(2,10);i++){
+        for(int j=1;j<=pow(2,10);j++){
+            a=pow(-1,(i+j))/(i+j);
+            
+        }
+    }
 }
-
